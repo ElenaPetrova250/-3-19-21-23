7077f99ad562bcf4f865a94ba6e250fa7e9168f9
ElenaPetrova250 <110457018+ElenaPetrova250@users.noreply.github.com>
2022-08-16 13:54:20 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-Console.Write("Введите координаты x1: ");
-int x1 = int.Parse(Console.ReadLine());
-Console.Write("Введите координаты x2: ");
-int x2 = int.Parse(Console.ReadLine());
-Console.Write("Введите координаты y1: ");
-int y1 = int.Parse(Console.ReadLine());
-Console.Write("Введите координаты y2: ");
-int y2 = int.Parse(Console.ReadLine());
-Console.Write("Введите координаты z1: ");
-int z1 = int.Parse(Console.ReadLine());
-Console.Write("Введите координаты z2: ");
-int z2 = int.Parse(Console.ReadLine());
-double distance = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
-Console.WriteLine ("Расстояние между отрезками равно {distance}");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int cube = int.Parse (Console.ReadLine());
+for (int i=1;i<cube;i++)
+Console.WriteLine(i*i*i);
